d8ba419900540a183c6b87743d9fda4bf5800d6d
SREDevOpsNL <dimi.martynov@gmail.com>
2024-01-07 21:23:19 +0100

    * Refactor show_details function to improve Treeview width and height * Calculate height of popup window and make it non-resizable. Make it easier to calculate the maximum length of dictionary-based values. * In popup window output data to TreeView and hardcode max height and calculate width for it based on the longest value * Add show_details event handler for double-click on row. Remove unused entry_frame, rename button to filter_button. * Move list of dicts to Pandas.DataFrame constructor * Remove commented code, add comments, remove white-spaces * Use variables for min and max width of root window

--- Tables/GUITables.py
+++ Tables/GUITables.py
@@ -9,32 +9,28 @@ import book_data  #Add all the books to the BookList
 book_data.add_books_to_BookList()
 
 # Create a DataFrame from the BookList
-list = [book.__dict__ for book in BookList.books]
-typeOfVar = type(list)
-df = pandas.DataFrame(list)
+df = pandas.DataFrame([book.__dict__ for book in BookList.books])
 
 # Create a root window and immediately hide it
 root = tkinter.Tk()
 
+# Assign minimum width and height of the root window
+min_width = 1652
+min_height = 326
 # Set the minimum window size
-root.minsize(1652, 326)
+root.minsize(min_width, min_height)
 
-# Create a Frame for the Entry widgets
-entry_frame = tkinter.Frame(root)
-button: tkinter.Button = None
+# Create a button to invoke the filter function
+filter_button: tkinter.Button = None
 
 # Function to handle Enter key press
 def on_enter(event):
-    button.invoke()
+    filter_button.invoke()
 
 # Create a new Frame for labels and entries
 label_entry_frame = tkinter.Frame(root)
-#label_entry_frame.pack(fill='both', expand=True)
-label_entry_frame.pack(fill='x')
-
-# Configure the grid to expand
-#label_entry_frame.grid_columnconfigure(0, weight=1)
-#label_entry_frame.grid_rowconfigure(0, weight=1)
+#label_entry_frame.pack(fill='both', expand=True) # expand=True to fill the entire root window and fill='both' to resize horizontally and vertically
+label_entry_frame.pack(fill='x') # to resize horizontally only
 
 # Configure the grid to expand
 for i in range(len(df.columns)):
@@ -66,8 +62,8 @@ def filter_table():
     pt.redraw()
 
 # Button to apply the filters
-button = tkinter.Button(label_entry_frame, text='Filter', command=filter_table) # Create a Button widget in the label_entry_frame
-button.grid(row=1, column=len(df.columns), sticky="ew") # Add 'Filter' button to label_entry_frame, to the right of the last entry. Use 'ew' to expand horizontally
+filter_button = tkinter.Button(label_entry_frame, text='Filter', command=filter_table) # Create a Button widget in the label_entry_frame
+filter_button.grid(row=1, column=len(df.columns), sticky="ew") # Add 'Filter' button to label_entry_frame, to the right of the last entry. Use 'ew' to expand horizontally
 
 # Create a Frame for the table
 table_frame = tkinter.Frame(root)
@@ -83,7 +79,7 @@ def resize_columns():
     num_columns = len(pt.model.df.columns)
     if width > num_columns:
         # Define the ratio for each column
-        column_ratios = {'title': 1.5, 'author': 0.5, 'synopsis': 2.7, 'publisher': 0.6, 'publish_date': 0.4,'page_count': 0.4, 'tags': 0.85}
+        column_ratios = {'title': 1.5, 'author': 0.5, 'synopsis': 1.7, 'publisher': 0.6, 'publish_date': 0.4,'page_count': 0.4, 'tags': 0.85}
         for col in pt.model.df.columns:
             ratio = column_ratios.get(col, 1)
             pt.columnwidths[col] = int(width * ratio // num_columns)
@@ -92,6 +88,39 @@ def resize_columns():
 
 # Bind the function to the <Configure> event of the root window
 root.bind('<Configure>', lambda _: resize_columns())
+
+def show_details(event):
+    # Create a new Toplevel window
+    details_window = tkinter.Toplevel(root)
+    # Create a Treeview widget
+    tree = ttk.Treeview(details_window, columns=('col', 'value'), show='')
+
+    # Get the indices of the selected rows
+    selected_indices = pt.multiplerowlist
+    # Get the selected rows from the DataFrame
+    selected_df = pt.model.df.iloc[selected_indices]
+    dict_of_details = [dict(row.items()) for _, row in selected_df.iterrows()][0]
+    # Calculate the maximum length of the values
+    max_length = max(len(str(value)) for value in dict_of_details.values())
+
+    # multiplier to adjust the width of the Treeview based on maximum length of the values
+    multiplier = 6
+    # Set the width of the Treeview and window
+    tree.column('value', width=max_length * multiplier)
+    width = max_length * multiplier + 202
+    max_height = len(dict_of_details) * 20
+    # Set the size of popup window 
+    details_window.minsize(width, max_height)
+    details_window.maxsize(width, max_height)
+    details_window.geometry(f'{width}x{max_height}')  # Adjust the size as needed
+    # Display the row details in the new window
+    for item in dict_of_details:
+            tree.insert('', 'end', values=(item, dict_of_details[item]))
+    # Adjust the height of the Treeview based on the number of items
+    tree.configure(height=len(tree.get_children()))
+    tree.pack()
+
+pt.bind('<Double-1>', show_details)
 
 # Show the table in the table_frame 
 pt.show()
